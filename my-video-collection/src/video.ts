f4b8c7dcf07cfac22662fe0911b9faabe2cc2f4c
Marentius <vetlenilseen@hotmail.com>
2024-07-15 13:30:07 +0200

    added almesus2024

--- my-video-collection/src/video.ts
+++ my-video-collection/src/video.ts
@@ -8,5 +8,9 @@ export const videos = [
   {
     src: 'https://drive.google.com/file/d/1003zR-Vr0nEYR80HyugGVIWnD2c_frSO/view?usp=sharing',
     title: 'Julebord 2022'
+  },
+  {
+    src: 'https://drive.google.com/file/d/1h8vwoeHRobPkaa1FWHUanK2w-EupkZ9N/view?usp=sharing',
+    title: 'Almesus 2024'
   }
 ];
