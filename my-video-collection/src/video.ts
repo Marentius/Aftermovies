cb93a6cbf47d34809ddd638ec2d13766e67f0c62
Marentius <vetlenilseen@hotmail.com>
2024-07-16 16:12:30 +0200

    changed to googleapi link

--- my-video-collection/src/video.ts
+++ my-video-collection/src/video.ts
@@ -1,6 +1,6 @@
 export const videos = [
   {
-    src: 'https://storage.cloud.google.com/almesus2024/Almesus2024.mp4',
+    src: 'https://storage.googleapis.com/almesus2024/Almesus2024.mp4',
     title: 'Almesus 2024',
     comment: ''
   }
